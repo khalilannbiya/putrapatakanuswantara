6d28eaaf173b0be4e929cb78af981f888c140562
khalilannbiya <syeichkhalil30@gmail.com>
2022-01-18 19:15:17 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-<div class="card-body">
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-            </div>
-        </div>
-
-        <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Login') }}
-                </button>
-
-                @if (Route::has('password.request'))
-                <a class="btn btn-link" href="{{ route('password.request') }}">
-                    {{ __('Forgot Your Password?') }}
-                </a>
-                @endif
-            </div>
-        </div>
-    </form>
-</div>
-</div>
-</div>
-</div>
-</div>
-@endsection --}}
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -97,7 +22,7 @@
             <div class="wrap-login100">
                 <div class="login100-form-title" style="background-image: url({{asset('LoginAssets/login.jpg')}});">
                     <span class="login100-form-title-1">
-                        TOUCHÉ ADMIN
+                        Login Admin
                     </span>
                 </div>
 
@@ -105,7 +30,8 @@
                     @csrf
                     <div class="wrap-input100 validate-input m-b-7" data-validate="Email is required">
                         <span class="label-input100">Email</span>
-                        <input class="input100 @error('email') is-invalid @enderror" type="email" name="email" placeholder="Enter your email" id="email" value="{{ old('email') }}" autofocus required>
+                        <input class="input100 @error('email') is-invalid @enderror" type="email" name="email"
+                            placeholder="Enter your email" id="email" value="{{ old('email') }}" autofocus required>
                     </div>
                     @error('email')
                     <p style="color:red; font-style:italic; font-size:11px; margin-top:-5px">{{ $message }}</p>
@@ -113,7 +39,8 @@
 
                     <div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
                         <span class="label-input100">Password</span>
-                        <input class="input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="Enter password" required>
+                        <input class="input100 @error('password') is-invalid @enderror" type="password" name="password"
+                            placeholder="Enter password" required>
                     </div>
 
                     <div class="flex-sb-m w-full p-b-30">
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,13 +10,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title> @yield('title') - Admin Touché</title>
+    <title> @yield('title') - Admin</title>
 
     <meta name="description" content="Ela Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="{{asset('user/images/logo.png')}}">
-    <link rel="shortcut icon" href="{{asset('user/images/logo.png')}}">
+    <link rel="apple-touch-icon" href="{{asset('user/images/logo-ppn.png')}}">
+    <link rel="shortcut icon" href="{{asset('user/images/logo-ppn.png')}}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -68,30 +68,29 @@
         <header id="header" class="header">
             <div class="top-left">
                 <div class="navbar-header">
-                    {{-- <a class="navbar-brand" href="{{url('/')}}"><img src="{{asset('ElaAdmin/images/logo.png')}}" alt="Logo"></a> --}}
-                    <a class="navbar-brand text-success" href="{{url('admin/abouts')}}"> <i class="fa fa-user-circle-o" style="font-size:34px"></i> <span class="font-weight-bold " style="font-size:26px">Administrator</span></a>
-                    <a class="navbar-brand hidden " href="{{url('/')}}"><img src="{{asset('ElaAdmin/images/logo2.png')}}" alt="Logo"></a>
+                    {{-- <a class="navbar-brand" href="{{url('/')}}"><img src="{{asset('ElaAdmin/images/logo.png')}}"
+                            alt="Logo"></a> --}}
+                    <a class="navbar-brand text-success" href="{{url('admin/abouts')}}"> <i class="fa fa-user-circle-o"
+                            style="font-size:34px"></i> <span class="font-weight-bold "
+                            style="font-size:26px">Administrator</span></a>
+                    <a class="navbar-brand hidden " href="{{url('/')}}"><img
+                            src="{{asset('ElaAdmin/images/logo2.png')}}" alt="Logo"></a>
                     <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
                 </div>
             </div>
             <div class="top-right">
                 <div class="header-menu">
                     <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="{{asset('user/images/logo.png')}}" alt="User Avatar">
+                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            <img class="user-avatar rounded-circle" src="{{asset('user/images/logo-ppn.png')}}"
+                                alt="User Avatar">
                         </a>
 
                         <div class="user-menu dropdown-menu">
-                            <a class="nav-link" href="#"><i class="fa fa-cog"></i>Ganti Password</a>
-                            {{-- <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                            <i class="fa fa-power-off"></i> Logout
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form> --}}
 
-                            <div class="nav-link" style="cursor:pointer" onclick="logout()" data-target="#modalLogout" data-toggle="modal">
+                            <div class="nav-link" style="cursor:pointer" onclick="logout()" data-target="#modalLogout"
+                                data-toggle="modal">
                                 <i class="fa fa-power-off"></i> Logout
                             </div>
                         </div>
@@ -143,7 +142,7 @@
             <div class="footer-inner bg-white">
                 <div class="row">
                     <div class="col-sm-6">
-                        Copyright &copy; 2021 Touché Web Development
+                        Copyright &copy; 2021 PT. Putra Pataka Nuswantara
                     </div>
                     <div class="col-sm-6 text-right">
                         Designed by <a href="https://colorlib.com">Colorlib</a>
